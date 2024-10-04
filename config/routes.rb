7605b6fe37ffd317ba67cb06222dc42f150c9e89
scbq <felipe.aguilera.fuentealba@gmail.com>
2024-10-04 12:51:11 -0300

    arreglando vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :peliculas
   resources :clientes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "clientes#index"
+  root "home#index"
 end
